e49f2f92d88552f059b8401400f7059f43ae7bcc
Roland Branten <roland.branten@gmail.com>
2022-04-01 13:13:43 +0200

    patch majority of packages

--- resources/assets/app.js
+++ resources/assets/app.js
@@ -1,6 +1,8 @@
 import App from 'components/App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './styles/App.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
